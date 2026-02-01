ce93ace23507c4ddb30da6f28cb46ff4ab649fed
Milan-Kurpershoek <1105163@hr.nl>
2026-02-01 13:19:23 +0100

    POST fix

--- routes/prehistoricAnimalsRouter.js
+++ routes/prehistoricAnimalsRouter.js
@@ -95,13 +95,13 @@ router.post("/", upload.single("prehistoricAnimal"), async (req, res) => {
     })
 
     if (!prehistoricAnimal) {
-        res.status(400).json({message: "All fields are required."})
+        res.status(404).json({message: "Resource not found"})
     } else {
         await prehistoricAnimal.save()
         res.status(201).json(prehistoricAnimal)
     }
     }catch (e){
-        res.status(400).json({message: "Invallid."})
+        res.status(400).json({message: "All fields are required"})
     }
 })
 
